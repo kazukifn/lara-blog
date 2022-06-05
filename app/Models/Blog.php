70103df750b1bb2f6ab50a1371f1dae1d29b0313
kazukifn <oknsdnhrkm56@gmail.com>
2022-06-05 21:58:14 +0900

    Blogモデルを作成

---
+++ app/Models/Blog.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Blog extends Model
+{
+    use HasFactory ,SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'content',
+        'picture_url',
+    ];
+
+    public function user() /**
+     * Get the user that owns the Blog
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
