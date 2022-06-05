2444e80309768d100c0a0aefe05a1f71128323f2
kazukifn <oknsdnhrkm56@gmail.com>
2022-06-05 20:13:48 +0900

    usersテーブルとblogsテーブルを作成するマイグレーションファイルを作成

--- database/migrations/2022_06_05_105537_create_blogs_table.php
+++ database/migrations/2022_06_05_105537_create_blogs_table.php
@@ -13,12 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('blogs', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->integer('role')->default(50);
+            $table->foreignId('user_id');
+            $table->string('content');
+            $table->string('picture_url')->default('0');
             $table->timestamps();
         });
     }
@@ -30,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('blogs');
     }
 };
